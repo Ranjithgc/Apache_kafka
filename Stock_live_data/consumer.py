32a701d66cc8583969f79d801aa69757f72a874d
Ranjithgc <Ranjith.amca.16@acharya.ac.in>
2021-08-09 20:06:12 +0530

    [Ranjithgc] Refactor: refactored by using pydoop library

---
+++ Stock_live_data/consumer.py
@@ -0,0 +1,15 @@
+"""
+@Author: Ranjith G C
+@Last Modified by: Ranjith G C
+@Last Modified time: 2021-08-07 
+@Title : Program Aim is to work stock live data and saving output to text file
+"""
+from kafka import KafkaConsumer
+import pydoop.hdfs as hdfs
+
+consumer = KafkaConsumer('testTopic')
+hdfs_path = 'hdfs://localhost:9000/kafka_stock_data/data.txt'
+for message in consumer:
+    values = message.value
+    with hdfs.open(hdfs_path, 'a') as f:    
+        f.write(values)
